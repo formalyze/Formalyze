5d84664df560edc754d39cb0fbe27b0249b3c039
formalyze <infoformalyze@gmail.com>
2025-06-16 14:02:03 +0530

    Gurpreet

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,9 +1,9 @@
-import React from 'react';  
+import React from "react";
 
 function App() {
   return (
     <div>
-      <h1>Hello, Nipun!</h1>
+      <h1>Hello, Gurpreet!</h1>
     </div>
   );
 }
