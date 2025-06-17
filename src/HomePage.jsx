3abadffebe40cd567b88af0d5f84803d80aee852
Formalyze <infoformalyze@gmail.com>
2025-06-17 15:23:17 +0530

    Message

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,10 +1,29 @@
 import React from "react";
-import card from "./gmycard";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import CreateFormPage from "./Pages/CreateForm.jsx";
+
+function Home() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/create-form");
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>Go to Create Form Page</button>
+    </div>
+  );
+}
+
 function App() {
   return (
-    <>
-      <card/>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-form" element={<CreateFormPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Pages/CreateForm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CreateFormPage() {
+  return (
+    <div>
+      <h1>Create Form Page</h1>
+      {/* Add your form here */}
+    </div>
+  );
+}
+
+export default CreateFormPage;
